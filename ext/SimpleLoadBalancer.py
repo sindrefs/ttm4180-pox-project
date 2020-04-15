1d2f2a7deb430c1723fe0e028a4a58b49413b88f
Sindre Sørensen <>
2020-04-15 10:30:54 +0200

    Client is not receiving arp reply. Trying to flood arp replies

--- ext/SimpleLoadBalancer.py
+++ ext/SimpleLoadBalancer.py
@@ -88,9 +88,9 @@ class SimpleLoadBalancer(object):
         msg = of.ofp_packet_out()  # create the necessary Openflow Message to make the switch send the ARP Reply
         msg.data = eth.pack()
 
-        # TODO: Change to outport (test) (OLD PORT WAS: of.OFPP_IN_PORT)
+        # TODO: Change to outport (test) (OLD PORT WAS: of.OFPP_IN_PORT or outport)
         msg.actions.append(of.ofp_action_output(
-            port=outport))  # Append the output port which the packet should be forwarded to.
+            port=of.OFPP_FLOOD))  # Append the output port which the packet should be forwarded to.
 
         msg.in_port = outport
         connection.send(msg)
